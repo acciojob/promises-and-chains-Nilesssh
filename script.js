b09a96f56c9c54f2f61ac9849293c7671a70bbc5
Nilesssh <103926034+Nilesssh@users.noreply.github.com>
2023-03-21 09:41:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function formData() {
+    var name = document.getElementById("name").value;
+	var age = document.getElementById("age").value;
+  console.log(name);
+  console.log(age);
+		if(age > 18){
+			console.log("nilesh")
+		}else{
+			console.log("154")
+		}
+}
