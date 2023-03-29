6cb3142d0089dc2e43ff2d0b3a30cbfb7a798c92
Nilesssh <103926034+Nilesssh@users.noreply.github.com>
2023-03-29 15:19:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function formData() {
 	var age = document.getElementById("age").value;
   console.log(name);
   console.log(age);
-		if(age > 18){
+		if(age >= 18){
 			window.alert(`Welcome, ${name}. You can vote.`)
 		}else{
 			window.alert(`Oh sorry ${name}. You aren't old enough.`)
